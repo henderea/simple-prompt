1dfb94fe25f2039b01239c2c1f886365d9a76610
Eric Henderson <henderea@gmail.com>
2019-04-16 11:26:01 -0400

    greatly reduce the bundle size by using fewer and simpler libraries.  down from 748K to 4K !!!

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
 #!/usr/bin/env node
 
-const inquirer = require('inquirer');
-const yargs = require('yargs');
+const readline = require('readline');
+const arg = require('arg');
 
-const options = yargs
-    .string('message')
-    .nargs('message', 1)
-    .argv;
+const options = arg(
+    {
+        '--message': String
+    },
+    {
+        permissive: true
+    }
+);
 
-const prompt = inquirer.createPromptModule({ output: process.stderr });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stderr
+});
 
-prompt({
-    type: 'input',
-    name: 'value',
-    message: options.message
-}).then(answers => {
-    process.stdout.write(answers.value);
+let message = options['--message'];
+if(! /\s+$/.test(message)) {
+    message = `${message} `;
+}
+
+const style = s => `\u001B[${s}m`;
+
+const reset = style(0);
+const bold = style(1);
+const green = style(32);
+
+message = `${green}?${reset} ${bold}${message}${reset}`
+
+rl.question(message, answer => {
+    process.stdout.write(answer);
+    rl.close();
 });
