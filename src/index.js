6b628b33043cc545d2e94d99455bc02d5c0cc8ed
Eric Henderson <henderea@gmail.com>
2023-09-28 13:19:44 -0400

    add eslint and apply fixes

--- src/index.js
+++ src/index.js
@@ -4,37 +4,37 @@ const readline = require('readline');
 const arg = require('arg');
 
 const options = arg(
-    {
-        '--message': String
-    },
-    {
-        permissive: true
-    }
+  {
+    '--message': String
+  },
+  {
+    permissive: true
+  }
 );
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stderr
+  input: process.stdin,
+  output: process.stderr
 });
 
 let message = options['--message'];
 if(! /\s+$/.test(message)) {
-    message = `${message} `;
+  message = `${message} `;
 }
 
-const style = s => `\u001B[${s}m`;
+const style = (s) => `\u001B[${s}m`;
 
 const reset = style(0);
 const bold = style(1);
 const green = style(32);
 
-message = `${green}?${reset} ${bold}${message}${reset}`
+message = `${green}?${reset} ${bold}${message}${reset}`;
 
 rl.addListener('SIGINT', () => {
-    process.exit(130);
+  process.exit(130);
 });
 
-rl.question(message, answer => {
-    process.stdout.write(answer);
-    rl.close();
+rl.question(message, (answer) => {
+  process.stdout.write(answer);
+  rl.close();
 });
